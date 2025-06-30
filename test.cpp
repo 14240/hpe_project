809f20f92a25e8ffa18c6a3e795582842b16e264
Yashwanth D <118149969+14240@users.noreply.github.com>
2025-06-30 18:37:55 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+using namespace std;
+
+// A simple C++ class
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int square(int a) {
+        return a * a;
+    }
+};
 
 int main() {
     // TODO: Add the required feature here
+    Calculator calc;
+    int x = 4, y = 5;
+
+    cout << "Sum: " << calc.add(x, y) << endl;
+    cout << "Square of " << x << ": " << calc.square(x) << endl;
+
     return 0;
 }
