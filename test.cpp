ee02011d04b378874adac8538a25d51dd68e8459
Yashwanth D <118149969+14240@users.noreply.github.com>
2025-06-30 18:52:00 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
 
-// A simple C++ class
-class Calculator {
-public:
-    int add(int a, int b) {
-        return a + b;
-    }
-
-    int square(int a) {
-        return a * a;
-    }
-};
-
 int main() {
     // TODO: Add the required feature here
     Calculator calc;
